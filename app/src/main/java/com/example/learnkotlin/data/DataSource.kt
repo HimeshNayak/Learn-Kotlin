d633f390d2042c6f4a1f55270758e695f5498c1d
HimeshNayak <himeshnayak015@gmail.com>
2021-05-26 14:46:46 +0530

    created dataSource for loading affirmations

---
+++ app/src/main/java/com/example/learnkotlin/data/DataSource.kt
@@ -0,0 +1,23 @@
+package com.example.learnkotlin.data
+
+import com.example.learnkotlin.R
+import com.example.learnkotlin.model.Affirmation
+
+class DataSource {
+
+    fun loadAffirmations() : List<Affirmation>{
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+                Affirmation(R.string.affirmation2),
+                Affirmation(R.string.affirmation3),
+                Affirmation(R.string.affirmation4),
+                Affirmation(R.string.affirmation5),
+                Affirmation(R.string.affirmation6),
+                Affirmation(R.string.affirmation7),
+                Affirmation(R.string.affirmation8),
+                Affirmation(R.string.affirmation9),
+                Affirmation(R.string.affirmation10)
+            )
+    }
+
+}
