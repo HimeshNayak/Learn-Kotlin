086c72da72ab292a646e47814cef103db3eda0f2
HimeshNayak <himeshnayak015@gmail.com>
2021-05-26 23:32:15 +0530

    replaced constraint to frame layout and added recyclerView

--- app/src/main/java/com/example/learnkotlin/AffirmationsScreen.kt
+++ app/src/main/java/com/example/learnkotlin/AffirmationsScreen.kt
@@ -3,6 +3,8 @@ package com.example.learnkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.learnkotlin.adapter.ItemAdapter
 
 import com.example.learnkotlin.data.DataSource
 
@@ -11,8 +13,15 @@ class AffirmationsScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_affirmations_screen)
 
-        val textView: TextView = findViewById(R.id.lengthText)
-        textView.text = DataSource().loadAffirmations().size.toString()
+        // Initialize data.
+        val myDataSet = DataSource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
 
     }
 }
