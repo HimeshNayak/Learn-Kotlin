d8d9c663a49126b430a923d056d448d57e0b3012
HimeshNayak <himeshnayak015@gmail.com>
2021-06-05 16:09:02 +0530

    added init code for unscramble app

---
+++ app/src/main/java/com/example/learnkotlin/UnscrambleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.learnkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UnscrambleActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unscramble)
+    }
+}
