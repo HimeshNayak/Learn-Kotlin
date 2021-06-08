80a2cae57d79981aa23155e407dda6bb38ae0889
HimeshNayak <himeshnayak015@gmail.com>
2021-06-08 15:53:44 +0530

    added retrofit dependencies

---
+++ app/src/main/java/com/example/learnkotlin/network/MarsApiService.kt
@@ -0,0 +1,27 @@
+package com.example.learnkotlin.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+
+    @GET("photos")
+   fun getPhotos(): String
+
+}
+
+object MarsApi {
+
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java) }
+
+}
