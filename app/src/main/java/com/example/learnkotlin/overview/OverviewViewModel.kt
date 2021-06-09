0ba1647cdb8e453d935a4e070933ae682de9a531
HimeshNayak <himeshnayak015@gmail.com>
2021-06-09 19:13:46 +0530

    fetched images from url and binded them to grid view

--- app/src/main/java/com/example/learnkotlin/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/learnkotlin/overview/OverviewViewModel.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.learnkotlin.overview
 
 import androidx.lifecycle.LiveData
@@ -21,7 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.learnkotlin.network.MarsApi
+import com.example.learnkotlin.network.MarsPhoto
 import kotlinx.coroutines.launch
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -29,10 +16,18 @@ import kotlinx.coroutines.launch
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the request status
-    val status: LiveData<String> = _status
+    val status: LiveData<MarsApiStatus> = _status
+
+    // Internally, we use a MutableLiveData, because we will be updating the List of MarsPhoto
+    // with new values
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+
+    // The external LiveData interface to the property is immutable, so only this class can modify
+    val photos: LiveData<List<MarsPhoto>> = _photos
+
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
      */
@@ -45,12 +40,15 @@ class OverviewViewModel : ViewModel() {
      * [MarsPhoto] [List] [LiveData].
      */
     private fun getMarsPhotos() {
+
         viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
             try {
-                val listResult = MarsApi.retrofitService.getPhotos()
-                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = MarsApiStatus.DONE
             } catch (e: Exception) {
-                _status.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
             }
         }
     }
